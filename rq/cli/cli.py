d6de0b4864c682d2d36dd797365da3c325ebad5b
rpkak <rpkak@users.noreply.github.com>
2021-07-25 08:05:39 +0200

    fix https://github.com/rq/rq/issues/1469#issuecomment-882317703

--- rq/cli/cli.py
+++ rq/cli/cli.py
@@ -60,7 +60,7 @@ shared_options = [
                  default=DEFAULT_CONNECTION_CLASS,
                  help='Redis client class to use'),
     click.option('--path', '-P',
-                 default='.',
+                 default=['.'],
                  help='Specify the import path.',
                  multiple=True)
 ]
